313d5258f43b935231c1420f4754c8f285db7666
Frantisek Urban <urbanf8897@gmail.com>
2022-12-20 15:25:27 +0100

    styling links and button

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,12 +1,27 @@
 import { HashLink as Link } from "react-router-hash-link"
+import { useNavigate } from "react-router-dom"
+import "./navbar.css"
+import logo from "../assets/svg/built-brand.svg"
+import { Button } from "../components/Button"
 
 export const Navbar = () => {
+
+    const navigate = useNavigate()
+
     return (
-        <div className="navbar">
-            <Link to="/"> Home </Link>
-            <Link to=""> Contact </Link>
-            <Link to=""> Browse Homes </Link>
-            <Link to="/sale-house"> Sale House </Link>
+        <div className="navbar container">
+            <div className="built-logo">
+                <img src={logo} alt="brand-logo" />
+            </div>
+            <div className="navbar-links">
+                <Link to="/"> Home </Link>
+                <Link to="/#contact"> Contact </Link>
+                <Link to="/#browse-homes"> Browse Homes </Link>
+                <Link to="/sale-house"> Sale House </Link>
+            </div>
+            <div className="navbar-buttons">
+                <Button onClick={()=>navigate("/")} name="Sign Up"/>
+            </div>
         </div>
     )
 }
