4054bbc53bc2165258571ccbab505646396213e0
Frantisek Urban <urbanf8897@gmail.com>
2022-12-20 12:00:33 +0100

    inicialized files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { HashLink as Link } from "react-router-hash-link"
+
 
 function App() {
   return (
     <div className="App">
+      
     </div>
   );
 }
